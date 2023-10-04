13b07f739b929fcd2a3550cc9c06b92fabb9b85b
Rodrigo dos Santos <rdsa.rdsa@gmail.com>
2023-10-04 19:50:56 -0300

    FInalizado o desafio de usar scanner

--- src/contaTerminal.java
+++ src/contaTerminal.java
@@ -1,5 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main (String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+
+       double saldo = 237.48;
+
+        System.out.println("Por favor, digite seu primeiro nome! ");
+            String nome = scanner.next();
+
+        System.out.println("Por favor, digite seu sobrenome! ");
+            String sobrenome = scanner.next();
+
+        System.out.println("Por favor, digite sua agência! ");
+            String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta! ");
+            int numero = scanner.nextInt();
+
+
+
+
+
+        
+        System.out.println("Olá, " + nome + sobrenome + ", obrigado por criar uma conta no nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + saldo + " já está disponível para saque");
+        
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
     }
 }
